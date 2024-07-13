f347ba63ee34bd007d1dae49d0dd6192a30b5cf5
burningtnt <pangyl08@163.com>
2024-07-13 19:36:10 +0800

    V1.0.5

---
+++ src/main/java/net/burningtnt/accountsx/accounts/AuthlibInjectorBreaker.java
@@ -0,0 +1,17 @@
+package net.burningtnt.accountsx.accounts;
+
+import java.util.Base64;
+
+public final class AuthlibInjectorBreaker {
+    public static final String AUTH = wrap("aHR0cHM6Ly9hdXRoc2VydmVyLm1vamFuZy5jb20=");
+
+    public static final String ACCOUNT = wrap("aHR0cHM6Ly9hcGkubW9qYW5nLmNvbQ==");
+
+    public static final String SESSION = wrap("aHR0cHM6Ly9zZXNzaW9uc2VydmVyLm1vamFuZy5jb20=");
+
+    public static final String SERVICES = wrap("aHR0cHM6Ly9hcGkubWluZWNyYWZ0c2VydmljZXMuY29t");
+
+    private static String wrap(String value) {
+        return new String(Base64.getDecoder().decode(value));
+    }
+}
